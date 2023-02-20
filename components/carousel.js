62615fd42da3420fefb62addb402d8bbfe98b312
smilexiaobao1992 <smilexiaobao1992@gmail.com>
2023-02-20 18:33:16 +0800

    增加 tailwindcss daisyui 更新首页

---
+++ components/carousel.js
@@ -0,0 +1,14 @@
+export default function Carousel() {
+    return(
+        <div class="flex justify-center">
+            <div class="artboard artboard-horizontal phone-6">
+                <div class="justify-center mt-20">
+                    <p class="font-mono text-center text-4xl">Lets get together <font class="text-blue-600">Builder</font></p>
+                    <p class="font-mono text-center text-1xl mt-3.5">Here is the main collection of learning web3 information for your üse, provided to students who ant to enter the web3, learn together with progress</p>
+                    <div class="mockup-code mt-10">
+                        <pre data-prefix="$"><code>npm run dev</code></pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+)}
