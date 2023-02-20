6bc0bd944227f26f35aab6b66e8f609d41422a3a
smilexiaobao1992 <smilexiaobao1992@gmail.com>
2023-02-20 18:44:01 +0800

    增加公共头部，修改首页

---
+++ components/section.js
@@ -0,0 +1,29 @@
+export default function Section() {
+    return(
+        // <div class="flex justify-center mt-20">
+        //   <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        //     <figure><img src="../images/chatgpt.jpeg" alt="ChatGPT" /></figure>
+        //         <div class="card-body">
+        //             <h2 class="card-title">OPENAI</h2>
+        //              <p>Join us in shaping the future of technology.</p>
+        //           <div class="card-actions justify-end">
+        //             <button class="btn btn-primary">Enter</button>
+        //         </div> 
+        //     </div>
+        //     </div>
+        // </div>
+        <div class="flex justify-center">
+            <div class="mt-20 mb-20 w-4/6">
+            <div class="card lg:card-side bg-base-100 shadow-xl max-h-96">
+                <figure><img src="../images/chatgpt.jpeg" alt="Album"/></figure>
+        <div class="card-body">
+                <h2 class="card-title">Introducing ChatGPT Plus</h2>
+                <p>We’re launching a pilot subscription plan for ChatGPT, a conversational AI that can chat with you, answer follow-up questions, and challenge incorrect assumptions.</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">Join</button>
+            </div>
+        </div>
+        </div>
+        </div>
+    </div>
+)}
