e43ffa93df8cd1cda3bb41215d1b88a0f5b38a40
smilexiaobao1992 <smilexiaobao1992@gmail.com>
2023-02-20 18:40:06 +0800

    增加css 增加首页

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import MyFooter from '../components/footer'
+import MyHeader from '../components/header'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MyHeader/>
+        <Component {...pageProps} />
+      <MyFooter/>
+    </>
+  )
 }
