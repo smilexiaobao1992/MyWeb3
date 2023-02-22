73f7357214fd2268d5e4aa90e4fd64dcd198de0c
smilexiaobao1992 <smilexiaobao1992@gmail.com>
2023-02-22 16:39:44 +0800

    缺失ga默认导出

--- pages/ga.js
+++ pages/ga.js
@@ -8,3 +8,8 @@ export const logPageView = () => {
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
 };
+const GaPage = () => {
+    return <h1>This is your GA page</h1>;
+  };
+  
+export default GaPage;
